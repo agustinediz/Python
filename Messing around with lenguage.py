1a8cf8358b870f609930247a26900bf5bb58f4cd
agustinediz <65187706+agustinediz@users.noreply.github.com>
2021-02-02 18:03:07 -0300

    Create Messing around with lenguage.py

---
+++ Messing around with lenguage.py
@@ -0,0 +1,42 @@
+#Loop code aimed to replace each vowel in a word or phrase with another word using that vowel. This type of game is know as "geringoso" in spanish.
+
+cadena = 'Geringoso'
+capadepenapa = ''
+
+for c in cadena:
+	if c == "a":
+	 capadepenapa = cadena.replace("a", "apa")
+	elif c == "e":
+	 capadepenapa = cadena.replace("e", "epe") 
+	elif c == "i":
+	 capadepenapa = cadena.replace("i", "ipi") 
+	elif c == "o":
+	 capadepenapa = cadena.replace("o", "opo") 
+	elif c == "u":
+	 capadepenapa = cadena.replace("u", "upu") 
+	print(capadepenapa)	
+### Geperingoso
+    Geripingoso
+    Geripingoso
+    Geripingoso
+    Geringoposopo
+    Geringoposopo
+    Geringoposopo ###
+
+
+
+cadena = "boligoma"
+capadepenapa = ""
+
+for c in cadena:
+	if c == "a":
+	 capadepenapa = cadena.replace("a", "apa")
+	elif c == "e":
+	 capadepenapa = cadena.replace("e", "epe") 
+	elif c == "i":
+	 capadepenapa = cadena.replace("i", "ipi") 
+	elif c == "o":
+	 capadepenapa = cadena.replace("o", "opo") 
+	elif c == "u":
+	 capadepenapa = cadena.replace("u", "upu") 
+	print(capadepenapa)	
