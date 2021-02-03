962a7e674949a83742bbf3c1dca6538c61c44427
agustinediz <65187706+agustinediz@users.noreply.github.com>
2021-02-03 10:44:35 -0300

    Update trees.py

--- trees.py
+++ trees.py
@@ -30,3 +30,53 @@ D = [float(arbol["diametro"]) for arbol in arboleda if arbol["nombre_com"] == es
 a = zip(H,D)
 nueva_lista = tuple(a)
 nueva_lista
+
+#Exploring the list
+
+import csv
+import arboles
+def leer_parque(archivo, parque):
+    f = open(archivo, "rt", encoding = "utf-8")
+    filas = csv.reader(f)
+    encabezados = next(filas)
+    fila = next(filas)
+    lista_parque = []
+    for fila in filas:
+        record = dict(zip(encabezados, fila))
+        if record["espacio_ve"] == parque:
+            arbol = {record["id_arbol"], record["nombre_com"]}
+            lista_parque.append(arbol)
+    return(lista_parque)
+
+lista_arboles = leer_parque("../arbolado-en-espacios-verdes.csv", "GENERAL PAZ")
+lista_arboles.split()
+#2.23
+def especies(lista_arboles):
+    especies = []
+    lista = tuple(lista_arboles)
+    lista2 = dict(lista)
+    for i in lista2:
+        lista_comun = lista2[str]
+        especies = lista_comun
+    return(especies)
+especies(lista_arboles)
+
+#ID and common names
+
+import csv
+def leer_parque(archivo, parque):
+    f = open(archivo, "rt", encoding = "utf-8")
+    filas = csv.reader(f)
+    encabezados = next(filas)
+    fila = next(filas)
+    lista_parque = []
+    for fila in filas:
+        record = dict(zip(encabezados, fila))
+        if record["espacio_ve"] == parque:
+            arbol = record["id_arbol"], record["nombre_com"]
+            lista_parque.append(arbol)
+    return(lista_parque)
+
+arboles = leer_parque("../arbolado-en-espacios-verdes.csv", "GENERAL PAZ")
+arboles
+len(arboles)
